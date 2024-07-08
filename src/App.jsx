b848f655cfcf033cab597731c25e3ab971417cc2
Marst313 <dlegoinyoman1@gmail.com>
2024-07-08 23:08:43 +0700

    add ga

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SharedLayout from "./layout/SharedLayout";
 import { About, Calculating, Home, Team, Map } from "./pages";
 
+import ReactGA from "react-ga4";
+const TRACKING_ID = "G-7FWD6TWSPG"; // your Measurement ID
+
 const App = () => {
+  useEffect(() => {
+    ReactGA.initialize(TRACKING_ID);
+    // Send pageview with a custom path
+    ReactGA.send({
+      hitType: "pageview",
+      page: "/",
+      title: "Landing Page",
+    });
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
